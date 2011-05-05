f5cdfea8f5a60d99846cb122eb4773da0d6ed791
Giacomo Bagnoli <g.bagnoli@asidev.com>
2011-05-05 17:23:16 +0200

    Implemented traversal (but still fallback to old app)
    --HG--
    extra : convert_revision : g.bagnoli%40asidev.com-20110505152316-g5uucedok3icf05v

--- aybu/core/__init__.py
+++ aybu/core/__init__.py
@@ -13,12 +13,16 @@ def main(global_config, **settings):
     """ This function returns a Pyramid WSGI application.
     """
     log.info("Starting application")
-    config = Configurator(root_factory=Root, settings=settings)
+    config = Configurator(root_factory=Root,
+                          request_factory=AybuRequest,
+                          settings=settings)
     # Add fallback to old pylons application
     pylons = get_pylons_app(global_config)
     add_fallback_to(config, pylons)
+
     setup_database(settings)
 
+    # configure views
     config.include(add_views)
 
     return config.make_wsgi_app()
@@ -26,6 +30,8 @@ def main(global_config, **settings):
 
 def setup_database(settings):
     log.info("Setting up models")
+    # FIXME: skip engine mapping as it's already been done by
+    # the legacy pylons app
     #engine = engine_from_config(settings, "sqlalchemy.")
     #sess, mtd = init_model(engine)
     from aybu.cms.model.meta import dbsession, metadata
@@ -34,5 +40,4 @@ def setup_database(settings):
 
 def add_views(config):
     config.add_static_view('static/pyramid', "aybu.core:static")
-
 
--- aybu/core/resources.py
+++ aybu/core/resources.py
@@ -1,3 +1,70 @@
+import logging
+from sqlalchemy import and_
+from sqlalchemy.orm.exc import NoResultFound
+
+
 class Root(object):
     def __init__(self, request):
+        from aybu.core.model.entities import Language, Menu
         self.request = request
+        self.log = logging.getLogger("%s.%s" % (self.__class__.__module__,
+                                                self.__class__.__name__))
+        self.log.debug("Starting Traversal for %s", request.path_info)
+        self.languages = { l.lang: l
+                            for l in Language.query.\
+                                      filter(Language.enabled == True).all() }
+        self.menus = Menu.query.all()
+
+    def __getitem__(self, part):
+        try:
+            # we need to use first as 'lang' attribute alone can be not unique
+            self.log.debug("Searching for lang '%s'", part)
+            lang = self.languages[part]
+            return NodeTraverser(self.request, lang=lang, parents=self.menus)
+
+        except NoResultFound:
+            return KeyError(part)
+
+
+class NodeTraverser(object):
+
+    def __init__(self, request, lang, parents):
+        self.request = request
+        self.lang = lang
+        self.parents = parents
+        self.parents_id = {p.id for p in parents}
+        self.log = logging.getLogger("%s.%s" % (self.__class__.__module__,
+                                                self.__class__.__name__))
+
+    def __getitem__(self, part):
+        from aybu.core.model.entities import Node, NodeInfo
+        self.log.debug("Next url part: %s", part)
+        url_part = part.replace(".html", "")
+        try:
+            next_node_q = Node.query.filter(
+                                Node.translations.any(
+                                    and_(NodeInfo.url_part == url_part,
+                                         NodeInfo.lang == self.lang)
+                                )
+                            ).filter(Node.parent_id.in_(self.parents_id))
+
+            if url_part != part:
+                self.log.debug("Url is a leaf, searching a nodeinfo")
+                # this is a leaf, so it must be one!
+                nodeinfo = next_node_q.one()
+                self.log.debug("Found %s", nodeinfo)
+                return nodeinfo
+
+            else:
+                self.log.debug("Not a leaf: searching for parents")
+                parents = next_node_q.all()
+                if not parents:
+                    raise NoResultFound()
+                self.log.debug("Found parents: '%s'", parents)
+                return NodeTraverser(self.request, lang=self.lang,
+                                     parents=parents)
+
+        except NoResultFound:
+            self.log.debug("No Result found for '%s'", part)
+            raise KeyError(part)
+
