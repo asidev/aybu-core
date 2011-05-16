9119e8394b13cd431fe1a22f410c3d0e56d517d1
Giacomo Bagnoli <g.bagnoli@asidev.com>
2011-05-16 10:26:23 +0200

    Use sqlsoup to access the database for theme setup during configuration. Fixes #550
    --HG--
    extra : convert_revision : g.bagnoli%40asidev.com-20110516082623-xljsnftaa5ro2sal

--- aybu/core/__init__.py
+++ aybu/core/__init__.py
@@ -6,7 +6,9 @@ from pyramid.wsgi import wsgiapp
 from pyramid_beaker import session_factory_from_settings
 from pyramid.settings import asbool
 
-#from sqlalchemy import engine_from_config
+from sqlalchemy import engine_from_config
+from sqlalchemy.ext.sqlsoup import SqlSoup
+
 #from aybu.core.model import init_model
 
 from aybu.core.request import AybuRequest
@@ -15,6 +17,15 @@ from aybu.core.dispatch import get_pylons_app
 from captchalib.pyramid import CaptchaView
 
 log = logging.getLogger(__name__)
+
+try:
+    import uwsgi
+except ImportError:
+    log.info("AYBU Starting")
+    uwsgi = None
+else:
+    log.info("AYBU Starting behind uWSGI")
+
 
 
 def main(global_config, **settings):
@@ -72,9 +83,18 @@ def setup_database(settings):
     # the legacy pylons app
     #engine = engine_from_config(settings, "sqlalchemy.")
     #sess, mtd = init_model(engine)
-    from aybu.cms.model.meta import dbsession, metadata
-    AybuRequest.dbsession = dbsession
-    AybuRequest.dbmetadata = metadata
+    def _setup_model():
+        from aybu.cms.model.meta import dbsession, metadata
+        AybuRequest.dbsession = dbsession
+        AybuRequest.dbmetadata = metadata
+
+    if uwsgi and uwsgi.numproc > 1:
+        # FIXME: when removing the fallback pylons app, we need to install the
+        # postfork hook here
+        #uwsgi.post_fork_hook = _setup_model
+        _setup_model()
+    else:
+        _setup_model()
 
 
 def add_subscribers(config):
@@ -99,12 +119,13 @@ def add_views(config):
 
 def setup_assets(config):
     """ Setup search paths for static files and for templates """
-    from aybu.core.model.entities import Theme, Setting
-    from aybu.cms.model.meta import dbsession
 
-    theme_name = dbsession.query(Setting.value).\
-            filter(Setting.name == 'theme_name').subquery()
-    theme = Theme.query.filter(Theme.name.in_(theme_name)).one()
+    # Use SqlSoup to access database to avoid requiring the entities to be
+    # mapped, which is not the case when we are in a multiprocess environment.
+    engine = engine_from_config(config.registry.settings, "sqlalchemy.")
+    db = SqlSoup(engine)
+    tname = db.settings.filter(db.settings.name == u'theme_name').one().value
+    theme = db.theme.filter(db.theme.name == tname).one()
 
     log.info("Adding static view for aybu")
     config.add_static_view('static', 'aybu.core:static')
@@ -115,7 +136,10 @@ def setup_assets(config):
     themes_paths = [ pkg_resources.resource_filename('aybu.core', 'templates') ]
     while theme:
         themes_inheritance_chain.insert(0, theme)
-        theme = theme.parent
+        if theme.parent_name:
+            theme = db.theme.filter(db.theme.name == theme.parent_name).one()
+        else:
+            theme = None
 
     for theme in themes_inheritance_chain:
         theme_static_spec = 'aybu.themes:%s/public/' % theme.name
