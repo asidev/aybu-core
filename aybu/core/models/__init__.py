b5e8dd8cf058c28b77004f0bb47974c308e2bc69
Giacomo Bagnoli <g.bagnoli@asidev.com>
2011-10-20 10:35:35 +0200

    Reworked imports, added __all__

--- aybu/core/models/__init__.py
+++ aybu/core/models/__init__.py
@@ -24,30 +24,31 @@ __all__ = ['Base', 'File', 'Image', 'Banner', 'Language',
 
 from logging import getLogger
 from aybu.core.models.base import Base
-from aybu.core.models.file import Banner
-from aybu.core.models.file import File
-from aybu.core.models.file import Image
+from aybu.core.models.file import (Banner,
+                                   File,
+                                   Image)
 from aybu.core.models.language import Language
-from aybu.core.models.node import ExternalLink
-from aybu.core.models.node import InternalLink
-from aybu.core.models.node import Menu
-from aybu.core.models.node import Node
-from aybu.core.models.node import Page
-from aybu.core.models.node import Section
-from aybu.core.models.translation import MenuInfo
-from aybu.core.models.translation import NodeInfo
-from aybu.core.models.translation import PageInfo
-from aybu.core.models.translation import SectionInfo
-from aybu.core.models.translation import ExternalLinkInfo
-from aybu.core.models.translation import InternalLinkInfo
-from aybu.core.models.setting import Setting
-from aybu.core.models.setting import SettingType
-from aybu.core.models.theme import Keyword
-from aybu.core.models.theme import Theme
-from aybu.core.models.user import Group
-from aybu.core.models.user import User
-from aybu.core.models.view import View
-from aybu.core.models.view import ViewDescription
+from aybu.core.models.node import (ExternalLink,
+                                   InternalLink,
+                                   Menu,
+                                   Node,
+                                   Page,
+                                   Section)
+from aybu.core.models.translation import (MenuInfo,
+                                          NodeInfo,
+                                          PageInfo,
+                                          SectionInfo,
+                                          ExternalLinkInfo,
+                                          InternalLinkInfo)
+from aybu.core.models.setting import (Setting,
+                                      SettingType)
+from aybu.core.models.theme import (Keyword,
+                                    Theme)
+from aybu.core.models.user import (User,
+                                   Group)
+from aybu.core.models.view import (View,
+                                   ViewDescription)
+
 from aybu.core.utils import get_object_from_python_path
 from sqlalchemy import engine_from_config
 from sqlalchemy.orm import class_mapper
@@ -56,6 +57,15 @@ from sqlalchemy.orm import sessionmaker
 import json
 
 log = getLogger(__name__)
+
+
+__all__ = ['populate', 'engine_from_config_parser', 'create_session',
+           'add_default_data', 'default_data_from_config',
+           'default_user_from_config', 'Base', 'Banner', 'Image', 'File',
+           'Language', 'ExternalLink', 'InternalLink', 'Menu', 'Node', 'Page',
+           'Section', 'MenuInfo', 'NodeInfo', 'PageInfo', 'SectionInfo',
+           'ExternalLinkInfo', 'InternalLinkInfo', 'Setting', 'SettingType',
+           'Keyword', 'Theme', 'User', 'Group', 'View', 'ViewDescription']
 
 
 def populate(config, data, config_section="app:main", session=None):
