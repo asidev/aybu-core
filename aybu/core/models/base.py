f8819861e301dcc5128f6efb4589af7574b98fa6
Giacomo Bagnoli <g.bagnoli@asidev.com>
2012-01-13 14:54:16 +0100

    Removed unused import

--- aybu/core/models/base.py
+++ aybu/core/models/base.py
@@ -16,7 +16,6 @@ See the License for the specific language governing permissions and
 limitations under the License.
 """
 
-from sqlalchemy.orm import class_mapper
 from sqlalchemy.orm import object_mapper
 from sqlalchemy.orm.exc import NoResultFound
 from sqlalchemy.orm.properties import ColumnProperty
